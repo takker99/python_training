22f0187b4a162b9ab5558e11066d7c60367d8951
takker99 <electronic.homuhomu@gmail.com>
2020-04-02 11:14:38 +0900

    :rotating_light:

--- knock003.py
+++ knock003.py
@@ -1,7 +1,6 @@
 # No.003
 # "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
 
-import string
 import re
 
 sentence = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."

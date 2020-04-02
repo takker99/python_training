bad62be74546d09f9b646c2c8f6dc61f97244a59
takker99 <electronic.homuhomu@gmail.com>
2020-04-02 11:11:12 +0900

    :recycle: make code shorter, using the regex module `re`

--- knock003.py
+++ knock003.py
@@ -2,8 +2,9 @@
 # "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
 
 import string
+import re
 
 sentence = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 
-result = [len(i if i[-1] in string.ascii_letters else i[:-1]) for i in sentence.split()]
+result = [len(re.sub(r"\W", "", i)) for i in sentence.split()]
 print(result)
